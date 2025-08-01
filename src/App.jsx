138e39315fc99bc9d7d92014b1e0999cf73f6a8c
Samuel Echefu <samuelnnechefu@gmail.com>
2025-07-31 23:51:50 -0700

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import "./App.css";
 const images = [
-  { imageSrc: "/photobooth.mp4", imageAlt: "image1" },
-  { imageSrc: "/beach.jpeg", imageAlt: "image2" },
-  { imageSrc: "/birthday.jpeg", imageAlt: "image3" },
-  { imageSrc: "/greenGrass.jpeg", imageAlt: "image4" },
-  { imageSrc: "/photobooth.mp4", imageAlt: "image5" },
-  { imageSrc: "/cactusbday.jpeg", imageAlt: "image6" },
-  { imageSrc: "/testPriya.JPG", imageAlt: "image7" },
-  { imageSrc: "/starbucks.jpeg", imageAlt: "image8" },
+  { imageSrc: "photobooth.mp4", imageAlt: "image1" },
+  { imageSrc: "beach.jpeg", imageAlt: "image2" },
+  { imageSrc: "birthday.jpeg", imageAlt: "image3" },
+  { imageSrc: "greenGrass.jpeg", imageAlt: "image4" },
+  { imageSrc: "photobooth.mp4", imageAlt: "image5" },
+  { imageSrc: "cactusbday.jpeg", imageAlt: "image6" },
+  { imageSrc: "testPriya.JPG", imageAlt: "image7" },
+  { imageSrc: "starbucks.jpeg", imageAlt: "image8" },
 ];
 const images1 = [
-  { imageSrc: "/babyshoepriya.jpeg", imageAlt: "image1" },
-  { imageSrc: "/mirror.jpeg", imageAlt: "image2" },
-  { imageSrc: "/carpriya_again.jpeg", imageAlt: "image3" },
-  { imageSrc: "/icongreen_priya.jpeg", imageAlt: "image4" },
-  { imageSrc: "/tspriya.jpeg", imageAlt: "image5" },
-  { imageSrc: "/seriouspriya.jpeg", imageAlt: "image6" },
-  { imageSrc: "/red_black.jpeg", imageAlt: "image7" },
-  { imageSrc: "/snakepriya.jpeg", imageAlt: "image8" },
+  { imageSrc: "babyshoepriya.jpeg", imageAlt: "image1" },
+  { imageSrc: "mirror.jpeg", imageAlt: "image2" },
+  { imageSrc: "carpriya_again.jpeg", imageAlt: "image3" },
+  { imageSrc: "icongreen_priya.jpeg", imageAlt: "image4" },
+  { imageSrc: "tspriya.jpeg", imageAlt: "image5" },
+  { imageSrc: "seriouspriya.jpeg", imageAlt: "image6" },
+  { imageSrc: "red_black.jpeg", imageAlt: "image7" },
+  { imageSrc: "snakepriya.jpeg", imageAlt: "image8" },
 ];
 const images2 = [
-  { imageSrc: "/funnybday.jpeg", imageAlt: "image1" },
-  { imageSrc: "/anothermirror.jpeg", imageAlt: "image2" },
-  { imageSrc: "/workpriya.jpeg", imageAlt: "image3" },
-  { imageSrc: "/photobooth.jpeg", imageAlt: "image4" },
-  { imageSrc: "/carpriya.jpeg", imageAlt: "image5" },
+  { imageSrc: "funnybday.jpeg", imageAlt: "image1" },
+  { imageSrc: "anothermirror.jpeg", imageAlt: "image2" },
+  { imageSrc: "workpriya.jpeg", imageAlt: "image3" },
+  { imageSrc: "photobooth.jpeg", imageAlt: "image4" },
+  { imageSrc: "carpriya.jpeg", imageAlt: "image5" },
   { imageSrc: "donut.jpeg", imageAlt: "image6" },
-  { imageSrc: "/stpaddys.jpeg", imageAlt: "image7" },
-  { imageSrc: "/sexy.jpeg", imageAlt: "image8" },
+  { imageSrc: "stpaddys.jpeg", imageAlt: "image7" },
+  { imageSrc: "sexy.jpeg", imageAlt: "image8" },
 ];
 const App = () => {
   return (
@@ -86,7 +86,7 @@ const App = () => {
           })}
         </div>
         <div className="content absolute top-0">
-          <header className=" rounded-2xl min-sm:p-4 max-sm:p-2  relative -z-[0.5] mt-24 h-fit">
+          <header className=" rounded-2xl min-sm:p-4 max-sm:p-2 relative -z-[0.5] mt-24 h-fit">
             <h1 className="min-sm:text-5xl text-2xl text-shadow-2xs pb-2 font-bold text-transparent bg-clip-text bg-gradient-to-br to-green-700 via-yellow-200 from-purple-700">
               Happy Girlfriend Day!!
             </h1>
